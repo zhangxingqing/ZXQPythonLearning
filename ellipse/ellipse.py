2771389de211fd8c78d380cd648a2903f2dc70be
Libinggen <libinggen@C02Q6GZAFVH5.local>
2017-08-08 22:45:45 +0800

    注释：编历事件

--- ellipse/ellipse.py
+++ ellipse/ellipse.py
@@ -8,6 +8,7 @@ while True:#创建一个事件处理循环
     for event in pygame.event.get():
         if event.type==QUIT:
             sys.exit()#设置关闭按键
+    for event in pygame.event.get():#编历pygame事件数组
     screen.fill((0,0,200))#使用颜色 0，0，200来清除屏幕
     #draw a ellipse
     color=255,0,255
