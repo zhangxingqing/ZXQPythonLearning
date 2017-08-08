567e4a5f7ad84705ed323dc763dff72ae6a6a28d
ZhangXingqing <zhangli_xingqing@163.com>
2017-08-08 22:31:58 +0800

    碰到边界变颜色

--- Rect/Rect.py
+++ Rect/Rect.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,sys
 from pygame.locals import  *
 pygame.init()
 screen=pygame.display.set_mode((600,500))
@@ -7,6 +7,7 @@ pos_x=300
 pos_y=250
 vel_x=2
 vel_y=1
+color = 255, 255, 0
 while True:
     for event in pygame.event.get():
         if event.type in (QUIT ,KEYDOWN ):
@@ -18,10 +19,17 @@ while True:
     #keep rectangle on the screen
     if pos_x>500 or pos_x <0:
         vel_x =-vel_x
-    if pos_y >400 or pos_y <0:
+    if pos_y >300 or pos_y <0:
         vel_y =-vel_y
+    if pos_x==500:
+        color=255,0,0
+    if pos_y==300:
+        color=0,255,0
+    if pos_x==0:
+        color=0,255,255
+    if pos_y==0:
+        color=255,255,255
     # draw the rectangle
-    color = 255, 255, 0
     width = 0 #solid fill
     pos = pos_x, pos_y, 100, 200
     pygame.draw.rect(screen, color, pos, width)
