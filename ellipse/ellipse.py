0c58a92fe2b7ee51358f430f70e583d2c76cbb52
ZhangXingqing <zhangli_xingqing@163.com>
2017-08-08 20:46:12 +0800

    注释

--- ellipse/ellipse.py
+++ ellipse/ellipse.py
@@ -1,20 +1,19 @@
 import pygame
 import sys
-from pygame.locals import *
-pygame.init()
-screen=pygame.display.set_mode((600,500))
-pygame.display.set_caption('Drawing Ellipse')
-width=4
+from pygame.locals import * #导入pygame中的所有常量
+pygame.init() #初使化pygame
+screen=pygame.display.set_mode((600,500))#获取对显示系统的访问 ，并创建一个窗口
+pygame.display.set_caption('Drawing Ellipse')#创建窗口的标题
 x=300
 y=250
-while True:
+while True:#创建一个事件处理循环
     for event in pygame.event.get():
         if event.type==QUIT:
-            sys.exit()
-    screen.fill((0,0,200))
+            sys.exit()#设置关闭按键
+    screen.fill((0,0,200))#使用颜色 0，0，200来清除屏幕
     #draw a ellipse
     color=255,0,255
-    width=0
-    position=250,150,300,200
-    pygame.draw.ellipse(screen,color,position,width)
-    pygame.display.update()
+    position=250,150,300,200#矩形的开始坐标为（250，150），矩形的大小为长300，高 200
+    width=1#椭圆线宽参数。如果线宽是0或省略，则填充
+    pygame.draw.ellipse(screen,color,position,width)#绘制椭圆
+    pygame.display.update()#屏幕刷新
